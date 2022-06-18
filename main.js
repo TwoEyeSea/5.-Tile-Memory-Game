d57d587b6ab7fecbe942322287d823523a44837b
M Alexander Moroney <alexmoroney@gmail.com>
2022-06-18 01:41:40 -0500

    Initial Development of game's logic
    -  Define individual tiles as constants.
    - Initial development of the "assignImage()". Function currently adds the 
       "comparing" class to the selected tile and removes the "clickable" class from the 
       selected tile.
    - Add imageArray - will contain constants that = image sources.
    -  Build "determineTile()"  to simplify code for selecting tiles. Avoiding repetitive if statements within the "assignImage()".

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
+// Define html elements as constants for javascript
+const tileA = document.querySelector("#tile-A");
+const tileB = document.querySelector("#tile-B");
+const tileC = document.querySelector("#tile-C");
+const tileD = document.querySelector("#tile-D");
+const tileE = document.querySelector("#tile-E");
+const tileF = document.querySelector("#tile-F");
+
+// Game
+function assignImage(tile) {
+  console.log("hello");
+  if (tile.classList.contains("clickable")) {
+    tile.classList.add("comparing");
+    tile.classList.remove("clickable");
+    console.log("I should only work once");
+  }
+}
+
+// Image array
+const imageArray = ["cat", "cat", "dog", "dog", "ocelot", "ocelot"];
+
 console.log("hello world");
+tileA.classList.add("test");
+console.log(document.querySelector("#tile-A").classList.contains("test"));
+
+// Functions
+// function to determine which tile is being selected
+function determineTile() {
+  tileA.addEventListener("click", () => {
+    assignImage(tileA);
+  });
+  tileB.addEventListener("click", () => {
+    assignImage(tileB);
+  });
+  tileC.addEventListener("click", () => {
+    assignImage(tileC);
+  });
+  tileD.addEventListener("click", () => {
+    assignImage(tileD);
+  });
+  tileE.addEventListener("click", () => {
+    assignImage(tileE);
+  });
+  tileF.addEventListener("click", () => {
+    assignImage(tileF);
+  });
+}
+
+determineTile();
