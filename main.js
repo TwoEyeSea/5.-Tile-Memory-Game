448e35f9278144a5e60b7086ad1a5533b77bcb6c
M Alexander Moroney <alexmoroney@gmail.com>
2022-06-18 13:40:26 -0500

    Add randomImage() + refactor code
    - randomImage() function operational - returns an available element from the imageArray at random and removes that element from the array. There are three pairs of elements in the array, this will be used to distribute element pairs to selected tiles at random.
    
    - Add TileObject() constructor function.  Refactor code to use object properties instead of html classes in logical operations. e.g. Removed addition of ".clickable" class from assignImage(), TileOject now has "isClickable = true" property.

--- main.js
+++ main.js
@@ -6,44 +6,95 @@ const tileD = document.querySelector("#tile-D");
 const tileE = document.querySelector("#tile-E");
 const tileF = document.querySelector("#tile-F");
 
+// Tile Objects
+const tile_A_Object = new TileObject(tileA);
+const tile_B_Object = new TileObject(tileB);
+const tile_C_Object = new TileObject(tileC);
+const tile_D_Object = new TileObject(tileD);
+const tile_E_Object = new TileObject(tileE);
+const tile_F_Object = new TileObject(tileF);
+
+console.log(tile_A_Object);
+console.log(tile_B_Object);
+console.log(tile_C_Object);
+console.log(tile_D_Object);
+console.log(tile_E_Object);
+console.log(tile_F_Object);
+
 // Game
-function assignImage(tile) {
-  console.log("hello");
-  if (tile.classList.contains("clickable")) {
-    tile.classList.add("comparing");
-    tile.classList.remove("clickable");
-    console.log("I should only work once");
-  }
-}
 
-// Image array
+// Arrays
 const imageArray = ["cat", "cat", "dog", "dog", "ocelot", "ocelot"];
-
-console.log("hello world");
-tileA.classList.add("test");
-console.log(document.querySelector("#tile-A").classList.contains("test"));
+// Image Array
+const resetArray = imageArray;
 
 // Functions
 // function to determine which tile is being selected
 function determineTile() {
   tileA.addEventListener("click", () => {
-    assignImage(tileA);
+    if (tile_A_Object.isClickable) {
+      assignImage(tile_A_Object);
+    }
   });
   tileB.addEventListener("click", () => {
-    assignImage(tileB);
+    if (tile_B_Object.isClickable) {
+      assignImage(tile_B_Object);
+    }
   });
   tileC.addEventListener("click", () => {
-    assignImage(tileC);
+    if (tile_C_Object.isClickable) {
+      assignImage(tile_C_Object);
+    }
   });
   tileD.addEventListener("click", () => {
-    assignImage(tileD);
+    if (tile_D_Object.isClickable) {
+      assignImage(tile_D_Object);
+    }
   });
   tileE.addEventListener("click", () => {
-    assignImage(tileE);
+    if (tile_E_Object.isClickable) {
+      assignImage(tile_E_Object);
+    }
   });
   tileF.addEventListener("click", () => {
-    assignImage(tileF);
+    if (tile_F_Object.isClickable) {
+      assignImage(tile_F_Object);
+    }
   });
 }
-
 determineTile();
+
+// Get random image from array while removing array elements.
+function randomImage(array) {
+  const arrayIndex = Math.floor(Math.random() * array.length);
+  const arrayElement = array[arrayIndex];
+  array.splice(arrayIndex, 1);
+  return arrayElement;
+}
+
+// Assign Image to tile
+function assignImage(tileObject) {
+  const imageIdentifier = randomImage(imageArray);
+  console.log("im responsive");
+
+  if (tileObject.isClickable) {
+    tileObject.comparing = true;
+    tileObject.isClickable = false;
+  }
+
+  // Logic to assign images to tile.
+
+  // const imageElement = document.createElement("img");
+  // imageElement.setAttribute("src", `${imageIdentifier}`);
+  // tile.appendChild(imageElement);
+  // console.log(imageArray);
+  // console.log();
+}
+
+//tile Objact Constructor Function
+function TileObject(tile) {
+  this.tile = tile;
+  this.isSelected = false;
+  this.isClickable = true;
+  this.comparing = false;
+}
