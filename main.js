d094bee2653341795f179cbc3f628175036b6fc6
M Alexander Moroney <alexmoroney@gmail.com>
2022-06-22 19:44:08 -0500

    Refactor code to simplify game function execution
    main.js:
    
    reorganize code to execute functions on application launch.
    
    - Add tileIdArray[].
    
    -  assignImageObject() removed from determineTile().
    
    -  determineTile() simplified to only passing tileObjects to tileGame().
    
    - Add new initTileObject().
    
    - initTileObject() contains: assignImageObject(), assignTile(), assignImageSource(). These functions will run upon application launch.
    
    -  assigntileId() takes tileID elements from the tileIdArray and adds the value to new tileID key within tileObject. Willl be used to add "id" attributes to each img element for comparison with the compareImages().
    
    - .visible class changed to .invisible
    
    main.css:
    
    - add styles for .invisible class.

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 // Define html elements as constants for javascript
-const tileA = document.querySelector("#tile-A");
-const tileB = document.querySelector("#tile-B");
-const tileC = document.querySelector("#tile-C");
-const tileD = document.querySelector("#tile-D");
-const tileE = document.querySelector("#tile-E");
-const tileF = document.querySelector("#tile-F");
+const tileA_div = document.querySelector("#tile-A");
+const tileB_div = document.querySelector("#tile-B");
+const tileC_div = document.querySelector("#tile-C");
+const tileD_div = document.querySelector("#tile-D");
+const tileE_div = document.querySelector("#tile-E");
+const tileF_div = document.querySelector("#tile-F");
 
 // Tile Objects
-const tile_A_Object = new TileObject(tileA);
-const tile_B_Object = new TileObject(tileB);
-const tile_C_Object = new TileObject(tileC);
-const tile_D_Object = new TileObject(tileD);
-const tile_E_Object = new TileObject(tileE);
-const tile_F_Object = new TileObject(tileF);
+const tile_A_Object = new TileObject(tileA_div);
+const tile_B_Object = new TileObject(tileB_div);
+const tile_C_Object = new TileObject(tileC_div);
+const tile_D_Object = new TileObject(tileD_div);
+const tile_E_Object = new TileObject(tileE_div);
+const tile_F_Object = new TileObject(tileF_div);
 
 // Image Object
 const imageObjectA1 = new ImageObject();
@@ -49,75 +49,132 @@ const tileGameProperties = {
 };
 
 // Arrays
+// ImageObjects
 const imageObjectArray = [imageObjectA1, imageObjectA2, imageObjectB1, imageObjectB2, imageObjectC1, imageObjectC2];
-//"dog", "dog","cat", "cat",
-const imageArray = ["ocelot", "mouse"];
+
+// Image Sources for <img> elements
+const imageSourceArray = ["dog", "dog", "cat", "cat", "ocelot", "mouse"];
+
+// tileId's to place within tileObjects and img elements
+const tileIdArray = ["tileA", "tileB", "tileC", "tileD", "tileE", "tileF"];
 
 // Image Array
-const resetArray = imageArray;
+const resetArray = imageSourceArray;
 
 // Functions
 // Function on launch
+initTileObjects();
 determineTile();
 
-//tile Object Constructor Function
+// TileObject Constructor Function
 function TileObject(tile) {
   this.tile = tile;
+  this.tileId = null;
   this.isSelected = false;
   this.isClickable = true;
   this.comparing = false;
-  this.imageID = true;
   this.imageObject = null;
 }
 
-// image Object Constructor Function
+// ImageObject Constructor Function
 function ImageObject() {
   const imageElement = document.createElement("img");
   this.imgElement = imageElement;
 }
 
+// Launch functions
+// Function to assign imageObjects to tileObjects and to assign image sources randomly to imageObject img elements
+function initTileObjects() {
+  assignImageObject(tile_A_Object);
+  assignTileId(tile_A_Object);
+  assignImageSource(tile_A_Object, tile_A_Object.imageObject);
+
+  assignImageObject(tile_B_Object);
+  assignTileId(tile_B_Object);
+  assignImageSource(tile_B_Object, tile_B_Object.imageObject);
+
+  assignImageObject(tile_C_Object);
+  assignTileId(tile_C_Object);
+  assignImageSource(tile_C_Object, tile_C_Object.imageObject);
+
+  assignImageObject(tile_D_Object);
+  assignTileId(tile_D_Object);
+  assignImageSource(tile_D_Object, tile_D_Object.imageObject);
+
+  assignImageObject(tile_E_Object);
+  assignTileId(tile_E_Object);
+  assignImageSource(tile_E_Object, tile_E_Object.imageObject);
+
+  assignImageObject(tile_F_Object);
+  assignTileId(tile_F_Object);
+  assignImageSource(tile_F_Object, tile_F_Object.imageObject);
+}
+// Function to assign tileId to tileObect
+function assignTileId(tileObject) {
+  tileObject.tileId = tileIdArray.shift();
+}
+
+//  Functions to assign imageObjects to tileObjects and Get random image sources from array for img elements.
+function assignImageObject(tileObject) {
+  tileObject.imageObject = imageObjectArray.shift();
+}
+// Logic to assign image sources to imageObect img html elements randomly.
+function assignImageSource(tileObject, imageObject) {
+  const tileImage = imageObject.imgElement;
+  const tileId = tileObject.tileId;
+  const imageSource = randomImageSource(imageSourceArray);
+  console.log(`i'm responsive guys :D`);
+
+  tileImage.setAttribute("class", "invisible");
+  tileImage.setAttribute("id", `${tileId}`);
+  tileImage.setAttribute("src", `${imageSource}`);
+  console.log(imageSourceArray);
+  tileObject.tile.appendChild(tileImage);
+}
+// Function to get random image
+function randomImageSource(array) {
+  // Logic for getting a random array image element
+  const arrayIndex = Math.floor(Math.random() * array.length);
+  const arrayElement = array[arrayIndex];
+  array.splice(arrayIndex, 1);
+
+  // Image ID for tile objects
+  return arrayElement;
+}
+
+// UI functions
 // function to determine which tile is being selected - passes specific tiles to the game function
 function determineTile() {
-  tileA.addEventListener("click", () => {
-    if (tile_A_Object.isClickable && !tile_A_Object.imageObject) {
-      assignImageObject(tile_A_Object);
+  tileA_div.addEventListener("click", () => {
+    if (tile_A_Object.isClickable) {
       tileGame(tile_A_Object);
     }
   });
-  tileB.addEventListener("click", () => {
-    if (tile_B_Object.isClickable && !tile_B_Object.imageObject) {
-      assignImageObject(tile_B_Object);
+  tileB_div.addEventListener("click", () => {
+    if (tile_B_Object.isClickable) {
       tileGame(tile_B_Object);
     }
   });
-  tileC.addEventListener("click", () => {
-    if (tile_C_Object.isClickable && !tile_C_Object.imageObject) {
-      assignImageObject(tile_C_Object);
+  tileC_div.addEventListener("click", () => {
+    if (tile_C_Object.isClickable) {
       tileGame(tile_C_Object);
     }
   });
-  tileD.addEventListener("click", () => {
-    if (tile_D_Object.isClickable && !tile_D_Object.imageObject) {
-      assignImageObject(tile_D_Object);
+  tileD_div.addEventListener("click", () => {
+    if (tile_D_Object.isClickable) {
       tileGame(tile_D_Object);
     }
   });
-  tileE.addEventListener("click", () => {
-    if (tile_E_Object.isClickable && !tile_E_Object.imageObject) {
-      assignImageObject(tile_E_Object);
+  tileE_div.addEventListener("click", () => {
+    if (tile_E_Object.isClickable) {
       tileGame(tile_E_Object);
     }
   });
-  tileF.addEventListener("click", () => {
-    if (tile_F_Object.isClickable && !tile_F_Object.imageObject) {
-      assignImageObject(tile_F_Object);
+  tileF_div.addEventListener("click", () => {
+    if (tile_F_Object.isClickable) {
       tileGame(tile_F_Object);
     }
   });
-}
-
-function assignImageObject(tileObject) {
-  tileObject.imageObject = imageObjectArray.shift();
 }
 
 // Tile game main function
@@ -125,23 +182,11 @@ function tileGame(tileObject) {
   //update tileGameProperties()
   updateGameProperties(tileObject);
 
-  //assign Image
-  const tileImage = tileObject.imageObject.imgElement;
-  const imageSource = assignRandomImage(imageArray, tileObject);
-  console.log(`i'm responsive guys :D`);
-  console.log(tileGameProperties.tileCount);
-
   if (tileObject.isClickable) {
     tileObject.comparing = true;
     tileObject.isClickable = false;
     highlightTile(tileObject);
   }
-
-  // Logic to assign images to tile.
-  tileImage.setAttribute("class", "visible");
-  tileImage.setAttribute("src", `${imageSource}`);
-  tileObject.tile.appendChild(tileImage);
-  console.log(imageArray);
   compareImages();
 }
 
@@ -161,18 +206,6 @@ function updateGameProperties(tileObject) {
   return (tileGameProperties.tile2 = tileObject);
 }
 
-// Get random image from array while removing array elements.
-function assignRandomImage(array, tileObject) {
-  // Logic for getting a random array image element
-  const arrayIndex = Math.floor(Math.random() * array.length);
-  const arrayElement = array[arrayIndex];
-  array.splice(arrayIndex, 1);
-
-  // Image ID for tile objects
-  tileObject.imageID = `${arrayElement}`;
-  return arrayElement;
-}
-
 //Function Highlight Tile
 function highlightTile(tileObject) {
   if (!tileObject.isClickable) {
@@ -180,23 +213,23 @@ function highlightTile(tileObject) {
     tileObject.tile.classList.toggle("comparing");
   }
 }
+// Function to compare images within the tileGameProperties Object
+// function compareImages() {
+//   if (tileGameProperties.tileCount === 2) {
+//     if (tileGameProperties.tile1.imageID === tileGameProperties.tile2.imageID) {
+//       console.log("Congratulations you're a winner!");
+//       return tileGameProperties.reset();
+//     }
+//     console.log("unfortunatley you lose");
+//     tileGameProperties.tile1.tile.classList.toggle("clickable");
+//     tileGameProperties.tile1.tile.classList.toggle("comparing");
+//     tileGameProperties.tile1.tile.classList.toggle("visible");
 
-function compareImages() {
-  if (tileGameProperties.tileCount === 2) {
-    if (tileGameProperties.tile1.imageID === tileGameProperties.tile2.imageID) {
-      console.log("Congratulations you're a winner!");
-      return tileGameProperties.reset();
-    }
-    console.log("unfortunatley you lose");
-    tileGameProperties.tile1.tile.classList.toggle("clickable");
-    tileGameProperties.tile1.tile.classList.toggle("comparing");
-    tileGameProperties.tile1.tile.classList.toggle("visible");
+//     tileGameProperties.tile2.tile.classList.toggle("clickable");
+//     tileGameProperties.tile2.tile.classList.toggle("comparing");
+//     tileGameProperties.tile2.tile.classList.toggle("visible");
 
-    tileGameProperties.tile2.tile.classList.toggle("clickable");
-    tileGameProperties.tile2.tile.classList.toggle("comparing");
-    tileGameProperties.tile2.tile.classList.toggle("visible");
-
-    return tileGameProperties.reset();
-  }
-  return;
-}
+//     return tileGameProperties.reset();
+//   }
+//   return;
+// }
